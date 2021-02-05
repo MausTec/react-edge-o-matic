8191fa076f05bfefbe75b35e4d6c007d178aeb40
Mau Abata <mauabata@gmail.com>
2021-02-04 21:29:22 -0600

    Updates

---
+++ precache-manifest.7cabfb2adf9b3d0919cc22949955b054.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa20cece7d62c619a24e165a39fdf5dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8441734094c0854d501a",
+    "url": "./static/css/main.8b84a203.chunk.css"
+  },
+  {
+    "revision": "e30390a8aa3602c314a5",
+    "url": "./static/js/2.2609983c.chunk.js"
+  },
+  {
+    "revision": "20da8ea4290bad58610c92d9d0508e18",
+    "url": "./static/js/2.2609983c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8441734094c0854d501a",
+    "url": "./static/js/main.23b8a8c9.chunk.js"
+  },
+  {
+    "revision": "07dbb8992c31ea2d803e",
+    "url": "./static/js/runtime-main.299c2212.js"
+  }
+]);
